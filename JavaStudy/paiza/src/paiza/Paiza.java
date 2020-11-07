a5e6d0d22a913b0a1917cd483fc77333984f3770
github IwaiSumire <opz09966@gmail.com>
2020-11-07 09:46:40 +0900

    試し
    練習

--- JavaStudy/paiza/src/paiza/Paiza.java
+++ JavaStudy/paiza/src/paiza/Paiza.java
@@ -11,12 +11,17 @@ public class Paiza {
         int seiseki = 0;
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("一行目に生徒の人数、合格点を入れてください");
         String line = sc.nextLine();
         String[] data = line.split(" ");
-
+        
         int ninzu = Integer.parseInt(data[0]);
         int goukaku = Integer.parseInt(data[1]);
-
+        
+        System.out.println("出席番号順にテストの点数と、欠席回数を入力してください");
+        System.out.println("欠席１回に対し、５点減点されます");
+        System.out.println("合格者の出席番号のみ出力します");
+        
         for (int i = 0; i < ninzu; i++) {
             String line2 = sc.nextLine();
             String[] data2 = line2.split(" ");
